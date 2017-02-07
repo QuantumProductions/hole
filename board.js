fbf5275d12b962cf833a209d0dfae75027663a18
quantumpotato <quantumpotato@gmail.com>
2017-02-06 23:06:51 -0500

    Coloring background

--- board.js
+++ board.js
@@ -8,24 +8,22 @@ class Board extends View {
   }
 
   draw(ctt, x, y, w, h, s) {
-    ctt.fillStyle = "blue";
+    ctt.beginPath();
+    ctt.fillStyle = Color.boardBg;
+    ctt.fillRect(x,y,w,h);
+
     const m = w / 30;
-    const tileR = (m * 4) / 2;
+    const size = (m * 6);
     
     for (var i = 0; i < s.length; i++) {
       var row = s[i];
       for (var j = 0; j < row.length; j++) {
         let tile = row[j];
-
-        ctt.fillStyle = Color.outline;
+        let tileX = x + (j * 5 * m);
+        let tileY = y + (i * 5 * m);
         ctt.beginPath();
-        ctt.arc(x + (j * (5 * m)), y + (i * (5 *m)), 0.9 * tileR, 2 * Math.PI, false);
-        ctt.fill();
-
-        ctt.fillStyle = Color[tile];
-        ctt.beginPath();
-        ctt.arc(x + (j * (5 * m)), y + (i * (5 *m)), 0.9 * tileR, 2 * Math.PI, false);
-        ctt.fill();
+        ctt.fillStyle = Color[s[i][j]];
+        ctt.fillRect(x + j * size + 0.05 * size,y + i * size + 0.05 * size, size * 0.9, size * 0.9);
       }
     }
     
--- color.js
+++ color.js
@@ -2,11 +2,19 @@
 
 class Color {
   static setup() {
-    Color.x = "#d35400";
-    Color.o =  "#16a085"
-    Color.empty = "#000000";
-    Color.bg = "#000000";
+    // Color.x = "#d35400";
+    // Color.x = "#e74c3c";
+    Color.x = "#51d8c8";
+    // Color.o = "#16a085"
+    Color.o = "#a5d266";
+    // Color.empty = "#000000";
+    // Color.empty = "#34495e";
+    Color.empty = "#576666";
+    Color.bg = "#4c5959";
+    Color.boardBg = "#4c5959";
+    // Color.bg = "#2980b9";
     Color.outline = "#CCCCCC";
+    Color.stroke1 = "#000000";
     // '#2980b9'
   }
 }
